1ab7c9793294c43dfd794d2d0d7a8e9297cf9ead
Cristina Alarcon <cralarco@c1r4s3.42london.com>
2025-03-03 11:59:20 +0000

    cleaned up

--- get_next_line.c
+++ get_next_line.c
@@ -1,107 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cralarco <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 11:14:19 by cralarco          #+#    #+#             */
+/*   Updated: 2025/03/03 11:28:35 by cralarco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *line;
-    char *buff;
-    int num_char;
+	static char	*line;
+	char		*buff;
+	int			num_char;
 
-    if (fd < 0 || BUFFSIZE <= 0)
-        return NULL;
-    buff = malloc(BUFFSIZE + 1);
-    if (!buff)
-        return (NULL);
-
-    num_char = read(fd, buff, BUFFSIZE);
-    if (num_char < 0)
-    {
-        free(buff);
-        return (NULL);
-    }
-    if (num_char == 0)
-        return get_last_line(&line, buff);
-       
-
-    return get_fragment_of_line(&line, buff, fd);
+	if (fd < 0 || BUFFSIZE <= 0)
+		return (NULL);
+	buff = malloc(BUFFSIZE + 1);
+	if (!buff)
+		return (NULL);
+	num_char = read(fd, buff, BUFFSIZE);
+	if (num_char < 0)
+	{
+		free(buff);
+		return (NULL);
+	}
+	if (num_char == 0)
+		return (get_last_line(&line, buff));
+	return (get_fragment_of_line(&line, buff, fd));
 }
 
-char *get_fragment_of_line(char **str_line, char *str_buff, int fd)
+char	*get_fragment_of_line(char **str_line, char *str_buff, int fd)
 {
-    char *new_line;
-    char *str_line_before_n;
-    char *str_line_after_n;
+	char	*new_line;
+	char	*str_line_before_n;
+	char	*str_line_after_n;
 
-    if (*str_line == NULL)
-        *str_line = "";
-
-    new_line = ft_strjoin(*str_line, str_buff);
-    free(str_buff);
-    *str_line = new_line;
-    if (has_new_line(*str_line))
-    {
-        str_line_before_n = strdup_before(*str_line, '\n');
-        str_line_after_n = strdup_after(*str_line, '\n');
-        free(*str_line);
-        *str_line = str_line_after_n;
-        return (str_line_before_n);
-    }
-    return get_next_line(fd);
+	if (*str_line == NULL)
+		*str_line = "";
+	new_line = ft_strjoin(*str_line, str_buff);
+	free(str_buff);
+	*str_line = new_line;
+	if (has_new_line(*str_line))
+	{
+		str_line_before_n = strdup_before(*str_line, '\n');
+		str_line_after_n = strdup_after(*str_line, '\n');
+		free(*str_line);
+		*str_line = str_line_after_n;
+		return (str_line_before_n);
+	}
+	return (get_next_line(fd));
 }
 
-char *get_last_line(char **str_line, char *str_buff)
+char	*get_last_line(char **str_line, char *str_buff)
 {
-    char *str_tmp;
-    char *str_line_before_n;
-    char *str_line_after_n;
-    char *str_line_before_z;
+	char	*new_line;
+	char	*str_line_before_n;
+	char	*str_line_after_n;
+	char	*str_line_before_z;
 
-
-   if (ft_strlen(*str_line))
-   {
-        if (has_new_line(*str_line))
-        {   
-            
-            *str_line = ft_strjoin(str_tmp, str_buff);
-            free(str_buff);
-            str_line_before_n = strdup_before(*str_line, '\n');
-            str_line_after_n = strdup_after(*str_line, '\n');
-            free(*str_line);
-            *str_line = str_line_after_n;
-            return (str_line_before_n);
-        }
-        
-        str_line_before_z = strdup_before(*str_line, '\0');
-        free(*str_line);
-        *str_line = NULL;
-        return (str_line_before_z);
-   }
-    
-   free (*str_line);
-   *str_line = NULL;
-   return (*str_line);
+	if (ft_strlen(*str_line))
+	{
+		new_line = ft_strjoin(*str_line, str_buff);
+		free(str_buff);
+		*str_line = new_line;
+		if (has_new_line(*str_line))
+		{
+			str_line_before_n = strdup_before(*str_line, '\n');
+			str_line_after_n = strdup_after(*str_line, '\n');
+			free(*str_line);
+			*str_line = str_line_after_n;
+			return (str_line_before_n);
+		}
+		str_line_before_z = strdup_before(*str_line, '\0');
+		free(*str_line);
+		*str_line = NULL;
+		return (str_line_before_z);
+	}
+	free (*str_line);
+	*str_line = NULL;
+	return (*str_line);
 }
-
-
-
-
-// int main()
-// {
-//     int fd;
-//     char *line;
-
-//     fd = open("text.txt", O_RDONLY);
-//     if (fd < 0)
-//     {
-//         perror("Error opening file");
-//         return (1);
-//     }
-
-//     while ((line = get_next_line(fd)))
-//     {
-//         printf("%s\n", line);
-//         free(line);
-//     }
-
-//     close(fd);
-//     return (0);
-// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,47 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cralarco <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 11:29:05 by cralarco          #+#    #+#             */
+/*   Updated: 2025/03/03 11:49:51 by cralarco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int has_new_line(char *str)
+int	has_new_line(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if(str[i] == '\n')
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int ft_strlen(const char *str)
+int	ft_strlen(const char *str)
 {
-    int counter;
+	int		counter;
 
-    counter = 0;
-    if (!str)
-        return (0);
-    while(str[counter] != '\0')
-        counter++;
-    return(counter);
+	counter = 0;
+	if (!str)
+		return (0);
+	while (str[counter] != '\0')
+		counter++;
+	return (counter);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
-    int j;
-	int	s1_len;
-	int	s2_len;
+	int		j;
+	int		s1_len;
+	int		s2_len;
 	char	*new_str;
-
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	new_str = malloc(s1_len + s2_len + 1);
-    if (!new_str)
-        return (NULL);
+	if (!new_str)
+		return (NULL);
 	i = 0;
-    j = 0;
+	j = 0;
 	while (i < s1_len)
 	{
 		new_str[i] = s1[i];
@@ -51,57 +62,55 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	{
 		new_str[i] = s2[j];
 		i++;
-        j++;
+		j++;
 	}
 	new_str[i] = '\0';
 	return (new_str);
 }
 
-char *strdup_before(char *str, char c)
+char	*strdup_before(char *str, char c)
 {
-    char *sub_str;
-    int i;
-    int len;
+	char *sub_str;
+	int i;
+	int len;
 
-   
-    len = 0;
-    i = 0;
-    while (str[len] && str[len] != c)
-        len++;
-    if (str[len] == c)
-        len++;
-    sub_str = malloc(len + 1);
-    if (!sub_str)
-        return (NULL);
-    
-    while (i < len)
-    {
-        sub_str[i] = str[i];
-        i++;
-    }
-    sub_str[len] = '\0';
-    return(sub_str);
+	len = 0;
+	i = 0;
+	while (str[len] && str[len] != c)
+		len++;
+	if (str[len] == c)
+		len++;
+	sub_str = malloc(len + 1);
+	if (!sub_str)
+		return (NULL);
+	while (i < len)
+	{
+		sub_str[i] = str[i];
+		i++;
+	}
+	sub_str[len] = '\0';
+	return (sub_str);
 }
 
-char *strdup_after(char *str, char c)
+char	*strdup_after(char *str, char c)
 {
-    char *sub_str;
-    int i;
-    int j;
-    int len;
+	char	*sub_str;
+	int		i;
+	int		j;
+	int		len;
 
-    i = 0;
-    j = 0;
-    len = 0;
-    while ( str[i] && str[i] != c)
-        i++;
-    if (str[i] == c)
-        i++;
-    sub_str = malloc(len + 1);
-    if (!sub_str)
-        return (NULL);
-    while (str[i] != '\0')
-        sub_str[j++] = str[i++];
-    sub_str[j] = '\0';
-    return sub_str;
+	i = 0;
+	j = 0;
+	len = 0;
+	while	(str[i] && str[i] != c)
+		i++;
+	if (str[i] == c)
+		i++;
+	sub_str = malloc(len + 1);
+	if (!sub_str)
+		return (NULL);
+	while (str[i] != '\0')
+		sub_str[j++] = str[i++];
+	sub_str[j] = '\0';
+	return (sub_str);
 }
